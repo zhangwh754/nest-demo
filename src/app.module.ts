d5faca8a3a4dce1654f4e743da597d1d44fa21ca
张文皓 <zhangwh754@163.com>
2023-07-11 15:11:56 +0800

    ✨ feat: 环境变量的使用

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), AuthModule],
   controllers: [],
   providers: [],
 })
