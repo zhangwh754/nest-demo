53ef14e3e60bc34d986be0b7c2553f52a9ba4e2f
张文皓 <zhangwh754@163.com>
2023-07-17 17:01:08 +0800

    ✨ feat: 权限守卫优化

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,42 +1,43 @@
 import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
-import { Observable } from 'rxjs'
 import type { Request } from 'express'
-// import { JwtService } from '@nestjs/jwt'
+import { JwtService } from '@nestjs/jwt'
 
 import { ROLES_KEY } from '../decorator'
 import { Role } from '../enum'
+import { JWTConstant } from 'src/app/plugin'
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(private reflector: Reflector, private jwtService: JwtService) {}
 
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
     ])
 
-    if (!requiredRoles) return true
+    if (!requiredRoles) return true // 没有使用装饰器，即无需权限的接口
 
     const request = context.switchToHttp().getRequest<Request>()
 
-    if (requiredRoles.includes(request.query.role as Role)) return true
-
     const token = this.extractTokenFromHeader(request)
     if (!token) {
+      throw new UnauthorizedException() // 未携带token，返回403
+    }
+    try {
+      const user = await this.jwtService.verifyAsync(token, {
+        secret: JWTConstant.secret,
+      })
+      request['user'] = user
+      return requiredRoles.includes(user.role)
+    } catch (error) {
       throw new UnauthorizedException()
     }
-    return validateRequest(request)
   }
 
   private extractTokenFromHeader(request: Request): string | undefined {
     const [type, token] = request.headers.authorization?.split(' ') ?? []
     return type === 'Bearer' ? token : undefined
   }
-}
-
-const validateRequest = request => {
-  console.log(request.session.role)
-  return true
 }
