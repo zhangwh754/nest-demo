297d6ac11a58cd3b1cfd2a41debde4cf1a1e4a14
张文皓 <zhangwh754@163.com>
2023-07-26 14:40:12 +0800

    ✨ feat: typeoram配置修改

--- src/app/config/typeorm.ts
+++ src/app/config/typeorm.ts
@@ -5,10 +5,10 @@ dotenv.config()
 export const TypeOrmConfig = {
   type: 'mysql',
   host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  entities: [],
+  autoLoadEntities: true,
   synchronize: true,
-  port: 3306,
 }
